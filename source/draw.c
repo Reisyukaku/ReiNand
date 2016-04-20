5e9dedeff5f7e24f68935b0a29c41c8c2388e446
Reisyukaku <asterma@yahoo.com>
2016-04-20 19:11:55 -0400

    Updated loader to read patches.dat, restructured codebase a lot, rewrote fs.c to be more like stdio, made option for compiling codebase for O3DS, more checks for splash screen, updated README for past few commits, many other little fixes

--- source/draw.c
+++ source/draw.c
@@ -8,8 +8,16 @@
 #include "fs.h"
 #include "memory.h"
 
+u8 clearScreen(void){
+    //Only clear screens if they are initialized
+    if(GPU_PDN_CNT == 1) return 0;
+    memset(fb->top_left, 0, 0x38400);
+    memset(fb->top_right, 0, 0x38400);
+    memset(fb->bottom, 0, 0x38400);
+    return 1;
+}
+
 void shutdownLCD(void){
-    
     //Clear LCDs
     *(vu32*)0x10202A44 = 0;
     *(vu32*)0x10202244 = 0;
@@ -21,15 +29,12 @@ void shutdownLCD(void){
     ((void (*)())*(vu32*)0x1FFFFFF8)();
 }
 
-void clearScreen(void){
-    memset(fb->top_left, 0, 0x38400);
-    memset(fb->top_right, 0, 0x38400);
-    memset(fb->bottom, 0, 0x38400);
-}
-
 void loadSplash(void){
-    clearScreen();
-    if(fileRead(fb->top_left, "/rei/splash.bin", 0)){
+    //If FB was clear, and the image exists, display
+    if(clearScreen()){
+        fopen("/rei/splash.bin", "rb");
+        fread(fb->top_left, 1, fsize());
+        fclose();
         u64 i = 0xFFFFFF; while(--i) __asm("mov r0, r0"); //Less Ghetto sleep func
     }
 }
